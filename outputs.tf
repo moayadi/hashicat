4f03f0e479f0da53caa907f45225ca290aabbd2a
MoayadI <moayad@hashicorp.com>
2020-11-03 23:44:41 +0800

    updates to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,4 +3,18 @@ output "catapp_url" {
   value = "http://${aws_eip.hashicat.public_dns}"
 }
 
+output "vpc_id" {
+  value =  aws_vpc.hashicat.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.hashicat.id
+}
+
+output "security_groups" {
+  value = aws_security_group.hashicat.id
+}
+
+
+
 
