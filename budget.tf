6f13a38a3155d4ebe493c5ae6e7164cc5d1603a7
MoayadI <moayad@hashicorp.com>
2020-11-08 21:59:40 +0800

    fixed module

--- budget.tf
+++ budget.tf
@@ -4,6 +4,6 @@ module "workspace_budget" {
 
   limit             = var.Limit
   time_period_start = var.time_period_start
-  subscriber_email = "admin@boundarynetworks.com.au"
+  subscriber_email = ["admin@boundarynetworks.com.au"]
 }
 
