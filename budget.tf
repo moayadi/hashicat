88c9232b4410e707db7f7e0eed6502fcff77c9b8
MoayadI <moayad@hashicorp.com>
2020-11-08 21:49:25 +0800

    fixed module

--- budget.tf
+++ budget.tf
@@ -2,6 +2,7 @@ module "workspace_budget" {
   source  = "app.terraform.io/moayadi/workspace-budget/aws"
   version = "0.1.5"
 
+  TFC_WORKSPACE_NAME = var.TFC_WORKSPACE_NAME
   limit             = var.Limit
   time_period_start = var.time_period_start
   subscriber_email = "admin@boundarynetworks.com.au"
