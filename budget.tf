1623e2083854ca46ec2b119d98ff39cad3ac01fa
MoayadI <moayad@hashicorp.com>
2020-11-08 22:36:33 +0800

    fixed email

--- budget.tf
+++ budget.tf
@@ -1,9 +1,9 @@
 module "workspace_budget" {
   source  = "app.terraform.io/moayadi/workspace-budget/aws"
-  version = "0.1.9"
+  version = "0.2.0"
 
   limit             = var.Limit
   time_period_start = var.time_period_start
-  subscriber_email = [var.Notification]
+  subscriber_email = var.Notification
 }
 
