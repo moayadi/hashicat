37b72d4c5e408c23b5d53c36bdd4bff8c4bbc9af
MoayadI <moayad@hashicorp.com>
2020-11-09 23:17:32 +0800

    update to notification

--- main.tf
+++ main.tf
@@ -220,18 +220,3 @@ module "workspace_budget" {
   subscriber_email = var.Notification
 }
 
-module "lambda-scheduler-stop" {
-  source  = "app.terraform.io/moayadi/lambda-scheduler-stop-start/aws"
-  version = "2.10.0"
-  name                           = "${var.TFC_WORKSPACE_NAME}_ec2_stop"
-  cloudwatch_schedule_expression = "cron(00 20 * * ? *)"
-  schedule_action                = "stop"
-  ec2_schedule                   = "true"
-  rds_schedule                   = "false"
-  autoscaling_schedule           = "false"
-  resources_tag                  = {
-    key   = "Environment"
-    value = "dev"
-  }
-  tags = local.common_tags
-}
