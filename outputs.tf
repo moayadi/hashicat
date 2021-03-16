bbb4526c0c79eaaa2502bfb6bbf9d7917aeb11bf
moayadi <42159604+moayadi@users.noreply.github.com>
2021-03-16 19:26:40 +0800

    updated outputs

--- outputs.tf
+++ outputs.tf
@@ -3,16 +3,20 @@ output "catapp_url" {
   value = "http://${aws_eip.hashicat.public_dns}"
 }
 
-output "vpc_id" {
-  value = aws_vpc.hashicat.id
-}
+# output "vpc_id" {
+#   value = aws_vpc.hashicat.id
+# }
 
-output "subnet_id" {
-  value = aws_subnet.hashicat.id
-}
+# output "subnet_id" {
+#   value = aws_subnet.hashicat.id
+# }
 
-output "security_groups" {
-  value = aws_security_group.hashicat.id
+# output "security_groups" {
+#   value = aws_security_group.hashicat.id
+# }
+
+output "ip_address" {
+  value = aws_eip.hashicat.public_ip
 }
 
 
