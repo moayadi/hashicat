c7267365ae636a633fb3091a71dbeee7a4e89e11
moayadi <42159604+moayadi@users.noreply.github.com>
2021-03-16 18:20:07 +0800

    update to vars

--- variables.tf
+++ variables.tf
@@ -104,3 +104,9 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "toStop" {
+  default = true
+  description = "tag used for auto shutdown lambda"
+  
+}
