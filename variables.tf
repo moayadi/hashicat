bfc12e5021cb270be080a2ee527a9b8a00ddd196
moayadi <42159604+moayadi@users.noreply.github.com>
2021-03-30 13:48:29 +0800

    update instance size

--- variables.tf
+++ variables.tf
@@ -82,7 +82,7 @@ variable "subnet_prefix" {
 
 variable "instance_type" {
   description = "Specifies the AWS instance type."
-  default     = "t3.xlarge"
+  default     = "t3.4xlarge"
 }
 
 // variable "admin_username" {
