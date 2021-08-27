0ed0fc3b88b2d5ccdb573637f8dcabcf71ec45e5
moayadi <42159604+moayadi@users.noreply.github.com>
2021-08-27 14:22:29 +0800

    removed backend

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "moayadi"
+# terraform {
+#   backend "remote" {
+#     hostname = "app.terraform.io"
+#     organization = "moayadi"
 
-    workspaces {
-      name = "prod-hashicat"
-    }
-  }
-}
+#     workspaces {
+#       name = "prod-hashicat"
+#     }
+#   }
+# }
